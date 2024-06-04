8c886e46ce7692915999c9cb2dfefb8a0a9a3625
jordanpeteroliver <130162684+jordanpeteroliver@users.noreply.github.com>
2024-06-04 16:27:32 +0000

    corrigindo erro de digitação

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   status = models.CharField(
-      max_leangth=2,
+      max_length=2,
       choices=Status,
       default=Status.DRAFT,
   )
