f65a16aebe67ce874334b5664f87506d5fba3837
jordanpeteroliver <130162684+jordanpeteroliver@users.noreply.github.com>
2024-06-09 23:38:12 +0000

    primeira página básica do blog
    página gerada dinamicamente

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 def post_list(request):
@@ -10,11 +11,18 @@ def post_list(request):
                 context={'posts': posts})
 
 
+#def post_detail(request, id):
+#  try:
+#    post = Post.published.get(id=id)
+#  except Post.DoesNotExist:
+#    raise Http404("No Post found.")
+#  return render(request=request,
+#                template_name='blog/post/detail.html',
+#                context={'post': post})
+
+
 def post_detail(request, id):
-  try:
-    post = Post.published.get(id=id)
-  except Post.DoesNotExist:
-    raise Http404("No Post found.")
+  post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
   return render(request=request,
                 template_name='blog/post/detail.html',
                 context={'post': post})
